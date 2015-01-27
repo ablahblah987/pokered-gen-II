5e75b257e7883f30409b2793ade54b3cb48b03e5
U-Daniel-PC\Daniel <corrnondacqb@yahoo.com>
2015-01-27 01:39:53 -0500

    More small sfx bug fixes

--- engine/hidden_object_functions7.asm
+++ engine/hidden_object_functions7.asm
@@ -72,10 +72,6 @@ asm_1e9b0: ; 1e9b0 (7:69b0)
 	ld a, RBSFX_02_5f
 	call PlaySound
 	call WaitForSoundToFinish
-.asm_1e9c2
-	ld a, [wc02a]
-	cp $b9
-	jr nz, .asm_1e9c2
 	ld a, $d3
 	ld [H_DOWNARROWBLINKCNT2], a ; $ff8c
 	call DisplayTextID
